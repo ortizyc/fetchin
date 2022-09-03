7724357180760f597b65e7a17a4e928b2a7bb98b
ortizyc <recuritis@gmail.com>
2022-09-04 04:45:32 +0800

    feat(initialize): initialize

--- src/locale/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as enUS } from './en-us'
-export { default as zhCN } from './zh-cn'
-export { default as zhTW } from './zh-tw'
--- src/locale/types.ts
+++
@@ -1,9 +0,0 @@
-import type { HttpStatus } from '../core'
-
-export type Locale = Record<HttpStatus, string>
-
-export interface LocaleConfig {
-  name: string;
-  ensign?: string;
-  locale: Locale;
-}
