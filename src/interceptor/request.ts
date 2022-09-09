72564689d0ab0173baadde5433fe45b0828d3ff9
Cphayim <i@cphayim.me>
2022-09-10 04:44:32 +0800

    feat: implements base type define and interceptors

---
+++ src/interceptor/request.ts
@@ -0,0 +1,30 @@
+import type { AxiosInstance } from 'axios'
+import type {
+  FetchinInterceptorOptions,
+  FetchinInterceptorRejected,
+  FetchinRequestInterceptorFulfilled,
+} from '../types'
+
+export function useRequestInterceptor(
+  axios: AxiosInstance,
+  onFulfilled?: FetchinRequestInterceptorFulfilled,
+  onRejected?: FetchinInterceptorRejected,
+  options?: FetchinInterceptorOptions,
+): number {
+  return axios.interceptors.request.use(onFulfilled, onRejected, options)
+}
+
+export function ejectRequestInterceptor(axios: AxiosInstance, id: number): void {
+  axios.interceptors.request.eject(id)
+}
+
+export const bearerAuthInterceptor: FetchinRequestInterceptorFulfilled = async (config) => {
+  if (config.getBearerToken) {
+    const token = await config.getBearerToken()
+    if (token) {
+      !config.headers && (config.headers = {})
+      config.headers.Authorization = `Bearer ${token}`
+    }
+  }
+  return config
+}
